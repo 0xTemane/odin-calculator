c7333b13211858d99f5c84607e5e70a127823f2c
0xTemane <170125706+0xTemane@users.noreply.github.com>
2026-05-28 19:38:16 +0200

    Create operate() within it the logic of operation

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-function add(num1, num2) {
-    console.log(num1 + num2);
-}
+let num1 = null;
+let num2 = null;
+let operator = "";
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+    }
+    
+    function add (num1, num2) {
+        console.log(num1 + num2);
+    }
 
-function subtract (num1, num2) {
-    console.log(num1 - num2);
-}
+    function subtract (num1, num2) {
+        console.log(num1 - num2);
+    }
 
-function multiply (num1, num2) {
-    console.log(num1 * num2);
-}
+    function multiply (num1, num2) {
+        console.log(num1 * num2);
+    }
 
-function divide (num1, num2) {
-    console.log(num1 / num2);
+    function divide (num1, num2) {
+        console.log(num1 / num2);
+    }
 }
